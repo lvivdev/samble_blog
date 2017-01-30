89454a8e6dbd66f9b8af9fb73565abe77f96ffd9
lvivdev <kl@alushta.net>
2017-01-30 19:55:26 +0200

    Add save function in contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,18 @@
 class ContactsController < ApplicationController
   
   def new
+
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    @contact.save
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :message)
   end
 
 end
