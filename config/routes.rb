f43d04c6ae9e1652d7368ba976b4ae3a2896c7b7
lvivdev <kl@alushta.net>
2017-01-31 14:43:47 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
   
-  resource :contacts, only: [:new, :create]
+  get 'contacts' => 'contacts#new'
+
+  resource :contacts, only: [:create]
   resources :articles
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
